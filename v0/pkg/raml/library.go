38e0ab2a977a1406df1f769ce6ab8de9ff1e2756
Elizabeth Harper <foxcapade@gmail.com>
2020-05-15 00:07:13 -0400

    typedef to pointer

--- v0/pkg/raml/library.go
+++ v0/pkg/raml/library.go
@@ -1,8 +1,8 @@
 package raml
 
 type Library struct {
-	Types         map[string]TypeDef `yaml:"types,omitempty"`
-	ResourceTypes []byte             `yaml:"resourceTypes,omitempty"`
-	Traits        []byte             `yaml:"traits,omitempty"`
-	Uses          map[string]string  `yaml:"uses,omitempty"`
+	Types         map[string]*TypeDef `yaml:"types,omitempty"`
+	ResourceTypes []byte              `yaml:"resourceTypes,omitempty"`
+	Traits        []byte              `yaml:"traits,omitempty"`
+	Uses          map[string]string   `yaml:"uses,omitempty"`
 }
